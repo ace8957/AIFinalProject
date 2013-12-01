4b24bd7b8b33b2d3a2c65eb70c59cd8df23bfb1d
Adam Wilford <canadianmoose8957@gmail.com>
2013-12-01 15:00:36 -0500

    Added file parsing and error measuring functions.

--- finalproject.py
+++ finalproject.py
@@ -3,6 +3,7 @@ import sys
 import math
 
 operators = ['+', '-', '*', '/', 'sin', '^']
+test_data = []
 
 class EquationNode:
     eq = ''
@@ -25,6 +26,33 @@ class EquationNode:
         print(self.eq)
         print(eval(self.eq))
 
+
+def read_file(filename):
+    f = open(filename, 'r')
+    line = f.readline()
+    if line != '':
+        test_data.append(line.replace('\n',''))
+    while line != '':
+        line = f.readline()
+        if line != '':
+            test_data.append(line.replace('\n',''))
+
+def calc_rms_error(equation, data_array):
+    squares = []
+    for entry in data_array:
+        l = entry.split(',')
+        x = int(l[0])
+        y = int(l[1])
+        ans = eval(equation.replace('x', str(x)))
+        squares.append((y-ans)**2)
+    ssum = 0
+    for square in squares:
+        ssum += square
+    ssum = ssum / len(squares)
+    return ssum ** 0.5
+
 if __name__ == "__main__":
-    root = EquationNode('+', '4', '5S')
+    root = EquationNode('+', '4', '5')
     root.parseTree()
+    read_file(sys.argv[1])
+    calc_rms_error('x+2', ['2,3','4,5'])
